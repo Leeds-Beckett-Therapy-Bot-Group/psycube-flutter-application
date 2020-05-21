723d343bc28028bae5534cc9dd9173c9269128af
Horatio Bennett <lucymccall20@gmail.com>
2020-05-21 20:31:14 +0100

    gradient backgrounds for new login/signup pages

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -8,6 +8,14 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Color(0xFF005073), Color(0xFF71c7ec)],
+        )
+      ),
+    );
   }
 }
